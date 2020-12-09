e2990cd8702fc2a8a21d9b99607f0be9de04f156
uniquethug <uniquethug@yeah.net>
2020-12-10 00:37:28 +0800

    add one sample

---
+++ sample/sample_1.py
@@ -0,0 +1,77 @@
+import pandas_datareader.data as web
+from datetime import datetime
+import backtrader as bt
+
+from pandas_datareader import data as pdr
+
+import yfinance as yf
+yf.pdr_override() #需要调用这个函数
+
+
+#回测期间
+start=datetime(2015, 1, 1)
+end=datetime(2015, 6, 1)
+
+# 获取数据
+# <bound method IndexOpsMixin.tolist of Index(['Open', 'High', 'Low', 'Close', 'Adj Close', 'Volume'], dtype='object')>
+df = pdr.get_data_yahoo("600519.SS", start=start, end=end)
+
+class my_strategy1(bt.Strategy):
+    #全局设定交易策略的参数
+    params=(
+        ('maperiod',20),
+           )
+
+    def __init__(self):
+        #指定价格序列
+        self.dataclose=self.datas[0].close
+        # 初始化交易指令、买卖价格和手续费
+        self.order = None
+        self.buyprice = None
+        self.buycomm = None
+
+        #添加移动均线指标，内置了talib模块
+        self.sma = bt.indicators.SimpleMovingAverage(
+                      self.datas[0], period=self.params.maperiod)
+    def next(self):
+        if self.order: # 检查是否有指令等待执行,
+            return
+        # 检查是否持仓
+        if not self.position: # 没有持仓
+            #执行买入条件判断：收盘价格上涨突破20日均线
+            if self.dataclose[0] > self.sma[0]:
+                #执行买入
+                self.order = self.buy(size=500)
+        else:
+            #执行卖出条件判断：收盘价格跌破20日均线
+            if self.dataclose[0] < self.sma[0]:
+                #执行卖出
+                self.order = self.sell(size=500)
+
+
+
+# 加载数据
+data = bt.feeds.PandasData(dataname=df, fromdate=start, todate=end)
+
+# 初始化cerebro回测系统设置
+cerebro = bt.Cerebro()
+#将数据传入回测系统
+cerebro.adddata(data)
+# 将交易策略加载到回测系统中
+cerebro.addstrategy(my_strategy1)
+# 设置初始资本为10,000
+startcash = 10000
+cerebro.broker.setcash(startcash)
+# 设置交易手续费为 0.2%
+cerebro.broker.setcommission(commission=0.002)
+
+d1=start.strftime('%Y%m%d')
+d2=end.strftime('%Y%m%d')
+print(f'初始资金: {startcash}\n回测期间：{d1}:{d2}')
+#运行回测系统
+cerebro.run()
+#获取回测结束后的总资金
+portvalue = cerebro.broker.getvalue()
+pnl = portvalue - startcash
+#打印结果
+print(f'总资金: {round(portvalue,2)}')
