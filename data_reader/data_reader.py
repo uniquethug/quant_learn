ffe168a60479833812aa51a4ec47cfca082ee7ae
uniquethug <uniquethug@yeah.net>
2020-12-19 21:49:13 +0800

    add volume analyze

--- data_reader/data_reader.py
+++ data_reader/data_reader.py
@@ -16,7 +16,12 @@ class DataReader:
     def get_history_data(self, *args, **kwargs):
         """
         :param args:
-        :param kwargs:code, start, end, columns, csv
+        :param kwargs:
+            code: ss->prefix code:0, szs->prefix code:1
+            start: yyyymmdd
+            end:  yyyymmdd
+            columns: []
+            csv: csv
         :return:
         """
         code = kwargs.get("code", "0600099")
@@ -42,14 +47,58 @@ class DataReader:
         return df[columns].sort_index()
 
     def get_stock_list(self):
-        pass
+        """
+        :return:ss_df, szs_df
+        """
+        ss_df = pandas.read_csv(os.path.join(os.path.dirname(__file__), "sse.csv"), encoding="GBK").rename(columns={"公司代码 ": "Code", "公司简称 ": "Name"})[["Code", "Name"]]
+        #szs = pandas.read_csv(os.path.join(os.path.dirname(__file__), "szse.csv"), encoding="GBK").rename(columns={"公司代码": "Code", "公司简称": "Name"})["Code", "Name"]
+        return ss_df
 
     @staticmethod
     def string2datetime64(date_string):
         """
-        :param date_string: y-m-d
+        :param date_string: yyyy-mm-dd
         :return:
         """
         t = time.strptime(date_string, "%Y-%m-%d")
         y, m, d = t[0:3]
         return numpy.datetime64(datetime.datetime(y, m, d))
+
+    def is_volume_incr(self, datas, start_date, mean_days=10):
+        start_date = self.string2datetime64(start_date)
+        mean_volume = datas[:start_date].sort_index(ascending=False).head(mean_days)["Volume"].mean()
+        volume_incr_flag = 0
+        for index, row in datas[start_date::].iterrows():
+            if row["Volume"] / mean_volume <= 1:
+                volume_incr_flag = 0
+            elif row["Volume"] / mean_volume > 1:
+                volume_incr_flag += 1
+
+            if volume_incr_flag > 3:
+                return True
+        return False
+    
+    def get_volume_incr_stock_list(self, code_list, start_date, end_date, analyze_start_date, mean_days):
+        for code in code_list:
+            code = str(code)
+            if code[0] == "6":
+                code = "0" + code
+            else:
+                code = "1" +code
+            if self.is_volume_incr(self.get_history_data(code=code, start=start_date, end=end_date), analyze_start_date, mean_days=mean_days):
+                print(code)
+    
+    def get_history_data_csv_by_code_list(self, code_list, start_date, end_date):
+        """
+        :param code_list: code list
+        :param start_date: yyyymmdd
+        :param end_date: yyyymmdd
+        :return:
+        """
+        for code in code_list:
+            code = str(code)
+            if code[0] == "6":
+                code = "0" + code
+            else:
+                code = "1" +code
+            self.get_history_data(code=code, start=start_date, end=end_date)
--- test/test_datareader.py
+++ test/test_datareader.py
@@ -1,5 +1,6 @@
 from data_reader.data_reader import DataReader
 import pytest
+import config
 
 
 @pytest.fixture(scope="module", autouse=True)
@@ -10,9 +11,33 @@ def datareader():
 def test_get_history_data(datareader):
     print("\n")
     df = datareader.get_history_data()
-    print(df)
     print(df.index)
+
+
+def test_get_history_data_with_csv(datareader):
+    print("\n")
+    df = datareader.get_history_data(csv=config.PROJECT_DIR + r"\tmp\stock_csv\0600099.csv")
+    print(df)
 
 
 def test_string2datetime64(datareader):
     print(datareader.string2datetime64("2018-01-01"))
+
+
+def test_volume_analysis(datareader):
+    df = datareader.get_history_data(csv=config.PROJECT_DIR + r"\tmp\stock_csv\0600099.csv")
+    print(datareader.is_volume_incr(df, "2018-05-28"))
+
+
+def test_stock_list(datareader):
+    datareader.get_stock_list()
+
+
+def test_get_history_data_csv_by_code_list(datareader):
+    code_list = datareader.get_stock_list()["Code"].to_list()
+    datareader.get_history_data_csv_by_code_list(code_list=code_list, start_date="20200101", end_date="20201218")
+
+
+def test_get_volume_incr_stock_list(datareader):
+    code_list = datareader.get_stock_list()["Code"].to_list()
+    datareader.get_volume_incr_stock_list(code_list, start_date="20200901", end_date="20201218", analyze_start_date="2020-12-01", mean_days=10)
